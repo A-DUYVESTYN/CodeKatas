73d9b4d135919dba6140dcfadca21a769d2b383b
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-10-18 16:12:17 -0400

    compare duplicate chars to encode

--- bitCounting.js
+++ bitCounting.js
@@ -1,5 +1,6 @@
 var countBits = function(n) {
   
-  return n.toString(2).split("").reduce( (prevNum, num) =>  prevNum + Number(num), 0)
+  return n.toString(2).split("").reduce( (prevNum, num) =>  prevNum + Number(num), 0);
 };
 
+console.log(countBits(10))
